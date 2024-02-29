76ae873901459cb5463794259f5dc32fdd71cc04
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2024-02-29 05:44:47 +0000

    ss

--- pages/uploadpdf.py
+++ pages/uploadpdf.py
@@ -27,36 +27,11 @@ def extract_images_from_pdf(filepath, file_dir):
       os.makedirs(img_path, exist_ok=True)
       captiolist = []
       with fitz.open(filepath) as doc:
-          for page_index in range(len(doc)):
-              page = doc[page_index]
+          for page in doc:  # iterate through the pages
+            pix = page.get_pixmap()  # render page to an image
+            pix.save(os.path.join(img_path,"page-%i.png" % page.number))  # store image as a PNG
 
-              for image_index, img in enumerate(page.get_images(full=True), start=1):
-                  xref = img[0]
-                  base_image = doc.extract_image(xref)
-                  image_bytes = base_image["image"]
-                  image_ext = base_image["ext"]
-                  image = Image.open(io.BytesIO(image_bytes))
-                  img_rect = page.get_image_rects(xref)[0]
-                  # Create a surrounding box that's 1.5 times the image area
-                  new_rect = fitz.Rect(
-                      img_rect.x0 - 0.25 * img_rect.width,
-                      img_rect.y0 - 0.25 * img_rect.height,
-                      img_rect.x1 + 0.25 * img_rect.width,
-                      img_rect.y1 + 0.25 * img_rect.height
-                  )
-
-                  imgfilepath = os.path.join("data", img_path, f"page_{page_index}_{image_index}.{image_ext}")
-                  # Extract text in the surrounding box
-                  text = page.get_text("text", clip=new_rect)
-                  captiolist.append({"Image_path":imgfilepath , "surroundingText": text})
-                  # If the image is in CMYK mode, invert the channels
-                  if image.mode == 'CMYK':
-                      inverted_channels = [channel.point(lambda p: 255 - p) for channel in image.split()]
-                      image = Image.merge('CMYK', inverted_channels)
-                      image = image.convert('RGB')
-
-                  image.save(os.path.join(img_path, f"page_{page_index}_{image_index}.{image_ext}"))
-                  img_rect = page.get_image_rects(xref)[0]
+            #    = page.get_image_rects(xref)[0]
 
 imageupload = st.file_uploader("Choose a PDF file", type="pdf")
 
