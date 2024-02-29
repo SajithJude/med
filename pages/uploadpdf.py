a7b8b9fab56b43376fd58dba0c00b72f4dd275c3
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2024-02-29 05:56:30 +0000

    jj

--- pages/uploadpdf.py
+++ pages/uploadpdf.py
@@ -17,7 +17,7 @@ from llama_index.core.schema import ImageNode
 from llama_index.multi_modal_llms.openai import OpenAIMultiModal
 
 openai_mm_llm = OpenAIMultiModal(
-    model="gpt-4-vision-preview", api_key=st.secrets["OPENAI_API_KEY"], max_new_tokens=9500
+    model="gpt-4-vision-preview", api_key=st.secrets["OPENAI_API_KEY"], max_new_tokens=3000
 )
 
 
