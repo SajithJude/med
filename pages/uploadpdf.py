47c10464a2465e0e50cde74763334b0cce17e001
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2024-02-29 05:55:47 +0000

    sss

--- pages/uploadpdf.py
+++ pages/uploadpdf.py
@@ -56,7 +56,7 @@ if imageupload is not None:
     st.write(dirs)
 
     image_documents = [
-        ImageDocument(image_path="data/pages/"+image_path) for image_path in dirs
+        ImageDocument(image_path=os.path.join("data/pages/",image_path)) for image_path in dirs
     ]
 
 
